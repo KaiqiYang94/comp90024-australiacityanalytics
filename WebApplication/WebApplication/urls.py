48b1070542eba127a225dd6d1060fd183dfac975
Xing Hu <xinghu0164@gmail.com>
2017-05-03 18:22:57 +1000

    add view template and transfer values into views

--- WebApplication/WebApplication/urls.py
+++ WebApplication/WebApplication/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from tweetswebsite import views as tweets_views
 
 urlpatterns = [
+    url(r'^', tweets_views.index),
     url(r'^admin/', admin.site.urls),
 ]
