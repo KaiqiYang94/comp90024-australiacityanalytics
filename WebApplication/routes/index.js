9604299785e15158fc4884ce8e860c98126e27c1
Xing Hu <xinghu0164@gmail.com>
2017-05-04 15:19:03 +1000

    add 'scenarios' and 'mapdemo'

--- WebApplication/routes/index.js
+++ WebApplication/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Australia City Analytics' });
+});
+
+router.get('/scenarios', function(req, res, next) {
+
+	
+  res.render('scenarios', { });
+});
+
+router.get('/mapdemo', function(req, res, next) {
+  res.render('mapdemo', { });
 });
 
 module.exports = router;
