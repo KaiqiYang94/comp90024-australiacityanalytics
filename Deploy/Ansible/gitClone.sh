a36fa6326c13a67f20d04e81e24d8591ec360508
Kaiqi Yang <kaiqiy@student.unimelb.edu.au>
2017-05-04 16:24:00 +1000

    [Deployment-Kaiqi] a bit

--- Deploy/Ansible/gitClone.sh
+++ Deploy/Ansible/gitClone.sh
@@ -1,3 +1,13 @@
 sudo apt install git
 
 git clone https://kaiqiypublic:kaiqiypublicpw@bitbucket.org/xingh1/comp90024-australiacityanalytics.git
+
+
+## deployment 
+
+
+sudo apt-get update
+
+sudo apt-get install git
+
+wget https://nodejs.org/dist/latest/node-v7.10.0-linux-x64.tar.gz
