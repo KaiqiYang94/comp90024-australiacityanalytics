cc13db0debfe0578a36ce8eb75b0d5129e45d5d0
Kaiqi Yang <kaiqiy@student.unimelb.edu.au>
2017-05-05 14:23:04 +1000

    [kaiqi] -Change bigtwitter as backgroud job

--- tweetsreader/run_2.sh
+++ tweetsreader/run_2.sh
@@ -1,5 +1,5 @@
 python DatasetRead.py
-python BigTwitterReader.py
+pm2 start BigTwitterReader.py --no-autorestart
 pm2 start TweetsSearchDownload.py
 
 #pm2 start BigTwitterReader.py --no-autorestart
