e23ee2a7c12fda7685bb83153bcbe73a3f558390
Che Chi <cche2@student.unimelb.edu.au>
2017-05-05 16:43:29 +1000

    This is just a simple example which can be used as reference

--- tweetsanalysis/create_view.py
+++ tweetsanalysis/create_view.py
@@ -4,9 +4,11 @@ couch = couchdb.Server()
 db = couch['tweets']
 
 design_doc = {
+#_id is the design document id
   "_id": "_design/inside",
   "language": "javascript",
   "views": {
+#the name below is the index of the view
     "inside": {
       #"map": "function (doc) {if(!doc.addressed){lon = doc.place.bounding_box.coordinates[0][0][0];lat = doc.place.bounding_box.coordinates[0][0][1];if(insideMelbourne(lat,lon)){emit(doc._id, doc);}}}function insideMelbourne(lat, lon){if(lon>=144.5937418 && lon<=145.5125288,-38.4338593&&lat>=-38.4338593 && lat<=-37.5112737){return true;}else{return false;}}"
       "map": "function (doc){emit(doc._id, doc.text);}"
