f7cacc44cc9ea1166215f62aa64f4ce63b78a09f
Che Chi <cche2@student.unimelb.edu.au>
2017-05-05 16:13:59 +1000

    Create View Automatically

---
+++ tweetsanalysis/create_view.py
@@ -0,0 +1,25 @@
+import couchdb
+
+couch = couchdb.Server()
+db = couch['tweets']
+
+design_doc = {
+  "_id": "_design/inside",
+  "language": "javascript",
+  "views": {
+    "inside": {
+      #"map": "function (doc) {if(!doc.addressed){lon = doc.place.bounding_box.coordinates[0][0][0];lat = doc.place.bounding_box.coordinates[0][0][1];if(insideMelbourne(lat,lon)){emit(doc._id, doc);}}}function insideMelbourne(lat, lon){if(lon>=144.5937418 && lon<=145.5125288,-38.4338593&&lat>=-38.4338593 && lat<=-37.5112737){return true;}else{return false;}}"
+      "map": "function (doc){emit(doc._id, doc.text);}"
+    }
+  }
+}
+if db['_design/inside'] is not None:
+	del db['_design/inside']
+
+doc_id, doc_rev = db.save(design_doc)
+print doc_id, doc_rev
+
+results = db.view('inside/inside')
+print len(results)
+for row in results:
+	pass
