500a39c32ff28b67bbf93d34d2d7d95fb41e17ef
Kaiqi Yang <kaiqiy@student.unimelb.edu.au>
2017-05-06 22:41:44 +1000

    [Kaiqi] -Set tweets limit to 1000

--- tweetsanalysis/CreateView/run.sh
+++ tweetsanalysis/CreateView/run.sh
@@ -9,13 +9,15 @@ python create_view_db_tweets_summary.py
 
 cd ..
 #need to run this under tweetsanalysis folder
-pm2 start TweetsAnalysis.py --restart-delay 600000 
+pm2 start TweetsAnalysis.py --restart-delay 60000 
 # python TweetsAnalysis.py
 
 #need to run this under tweetsanalysis folder
-pm2 start TweetsSummary.py --restart-delay 600000 
+pm2 start TweetsSummary.py --restart-delay 60000 
 # python TweetsSummary.py
 
 #monitor the number of processed tweets
 #curl -X GET "http://localhost:5984/tweets/_design/tweets_analysis/_view/monitor_processed_number"
+
+
 
