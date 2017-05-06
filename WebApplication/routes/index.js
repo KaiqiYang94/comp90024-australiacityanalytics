b31a395b50020b177e062b031464d8887ba4e927
Xing Hu <xinghu0164@gmail.com>
2017-05-06 12:42:19 +1000

    read test data from couchdb

--- WebApplication/routes/index.js
+++ WebApplication/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,13 +8,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/scenarios', function(req, res, next) {
-
-	
-  res.render('scenarios', { });
+  
+  var content_url = 'http://admin:password@127.0.0.1:5984/dataset_ier/seifa_ier_aust_sa2.fid-6ec8c59a_15bc3c26fba_6344'
+  
+  //res.render('scenarios', { title:'Content', msg: content});
+  request(content_url, function(error, response, body){
+        //Error Handling
+     if(!error && response.statusCode == 200){
+            console.log(body);
+            
+            res.render('scenarios', {title: 'scenarios', msg: body});
+        } else {
+            res.render('scenarios', {title: JSON.stringify(error)});
+            console.log("wrong");
+        }
+    });
 });
+
 
 router.get('/mapdemo', function(req, res, next) {
   res.render('mapdemo', {title: 'IEO' });
 });
 
 module.exports = router;
+ 
