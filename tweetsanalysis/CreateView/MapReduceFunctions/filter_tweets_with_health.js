d0e813a5796b26147dc88fd7f87c9d2421af68d4
Xing Hu <xingh1@student.unimelb.edu.au>
2017-05-07 23:23:01 +1000

    add 'health' keywords in map-reduce view

--- tweetsanalysis/CreateView/MapReduceFunctions/filter_tweets_with_health.js
+++ tweetsanalysis/CreateView/MapReduceFunctions/filter_tweets_with_health.js
@@ -1,6 +1,21 @@
 function (doc) {
   if(doc.addressed){
-    topics = ['health', 'body', 'energy', 'fitness'];
+    topics = ['health', 'body', 'energy', 'fitness','ache','acne','aleve','ankle','allergy','anxiety','antibiotics',
+              'appetite','aspirin','asthma','arthrities','backache','bedtime','benadryl','burised','burning','anxious',
+              'caffeine','cancer','colds','cough','contagious','congestion','cure','cured','dealing','dehydrated','dental',
+              'depression','diabetes','dieting','dizzy','dizziness','doctor','dose','drained','earache','elbow','emergency',
+              'exercise','exhausted','excesdrin','faint','fatigue','fever','flu','fluids','h1n1','germs','heal','headaches',
+              'heal','healed','hearthurn','hiccups','hives','hospital','hungover','hurtin','hurts','hurting','ibuprofen',
+              'ick','ill','illness','infected','infection','inhaler','insomnia','intense','irritated','insurance','itch',
+              'itching','jaw','kidney','killing','knee','lasik','limping','lump','lung','lungs','massage','medication','medicine',
+              'meds','mri','muscles','nasal','neck','nose','nurse','nyquil','ouch','pain','painkiller','pains','panadol','paracetamol',
+              'physical','physically','pill','pills','pneumonia','pollen','prescription','puffy','rash','recover','recovered',
+              'recovering','rehab','remedies','remedy','respiratory','resting','rest','runny','scratchy','sick','sicker','sickness',
+              'sneeze','sneezed','sniffles','snot','sore','spasms','spine','sprain','steroids','stomache','stomachache','strep',
+              'stroke','stuffy','funburn','surgeon','surgery','swelling','swollen','symptoms','tension','throat','throats','throbbing',
+              'thyroid','tiredness','tissues','tonsillitis','tonsils','tooth','toothache','torn','treatment','tumor','tylenol','ugh',
+              'ulcer','unbearable','uncomfortable','unwell','vaccine','veins','vertigo','vicodin','viral','vision','vitamins','vomit',
+              'vomiting','wheezing','wrist'];
     var hasTopics = findTopics(topics, doc);
     if(hasTopics){
       emit([doc.suburb, doc.sentiment], 1);
