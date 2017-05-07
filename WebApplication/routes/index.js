041bb2332cad3779cc491eda9163eb000b3a73c9
Xing Hu <xinghu0164@gmail.com>
2017-05-07 18:21:57 +1000

    all scenario pages finished

--- WebApplication/routes/index.js
+++ WebApplication/routes/index.js
@@ -220,10 +220,72 @@ router.get('/scenarios/ier', function(req, res, next) {
 
 //for health page
 //data need: 1. aurin- 
-//           2. tweets- topic
+//           2. tweets- health-positive
+//           3. tweets- health-negative
 router.get('/scenarios/health', function(req, res, next) {
 
-    res.render('health', { title: 'Health' });
+    var suburbs_aurin = []
+    var scores_aurin = []
+    var suburb_pos_tweets = []
+    var suburb_neg_tweets = []
+    var scores_pos_tweets = []
+    var scores_neg_tweets = []
+
+    var aurin_url = 'http://admin:password@127.0.0.1:5984/dataset_self_assessed_health/_design/health_analysis/_view/poor_selfassessed_health_rate?limit=10;descending=True'
+    var tweets_pos_url = 'http://admin:password@127.0.0.1:5984/tweets_summary/_design/tweets_summary/_view/health_positive_rate?limit=10;descending=True'
+    var tweets_neg_url = 'http://admin:password@127.0.0.1:5984/tweets_summary/_design/tweets_summary/_view/health_negative_rate?limit=10;descending=True'
+
+    // read 1
+    rp(aurin_url)
+        .then(function(response) {
+            var obj = JSON.parse(response);
+            for (var row in obj['rows']) {
+
+                var score = obj['rows'][row]['key'];
+                var suburb = obj['rows'][row]['value'];
+                suburbs_aurin.push(suburb);
+                scores_aurin.push(score);
+            }
+            // read 2
+            return rp(tweets_pos_url)
+        })
+        .then(function(response) {
+            var obj = JSON.parse(response);
+            for (var row in obj['rows']) {
+
+                var score = obj['rows'][row]['key'];
+                var suburb = obj['rows'][row]['value'];
+                suburb_pos_tweets.push(suburb);
+                scores_pos_tweets.push(score);
+            }
+            // read 2
+            return rp(tweets_neg_url)
+        })
+
+    .then(function(response) {
+        var obj = JSON.parse(response);
+        for (var row in obj['rows']) {
+
+            var score = obj['rows'][row]['key'];
+            var suburb = obj['rows'][row]['value'];
+            suburb_neg_tweets.push(suburb);
+            scores_neg_tweets.push(score);
+        }
+        res.render('health', {
+            chart1: 'AURIN- Top 10 suburbs with highest poor self assessed health rate',
+            suburbs_aurin: JSON.stringify(suburbs_aurin),
+            scores_aurin: JSON.stringify(scores_aurin),
+            chart2: 'TWEETS- Top 10 suburbs with highest positive rate of health',
+            suburb_pos_tweets: JSON.stringify(suburb_pos_tweets),
+            scores_pos_tweets: JSON.stringify(scores_pos_tweets),
+            chart3: 'TWEETS- TOP 10 Cities with highest negative rate of health',
+            suburb_neg_tweets: JSON.stringify(suburb_neg_tweets),
+            scores_neg_tweets: JSON.stringify(scores_neg_tweets)
+        });
+        return Promise.resolve();
+
+    }).catch(function(err) { console.log("error: " + err) });
+
 });
 
 //for Volunteer page
@@ -231,7 +293,48 @@ router.get('/scenarios/health', function(req, res, next) {
 //           2. tweets- semantic
 router.get('/scenarios/volunteer', function(req, res, next) {
 
-    res.render('volunteer', { title: 'volunteer' });
+    var suburbs_aurin = []
+    var scores_aurin = []
+    var suburb_tweets = []
+    var scores_tweets = []
+
+    var aurin_url = 'http://admin:password@127.0.0.1:5984/dataset_community_strength/_design/voluntary_work_analysis/_view/voluntary_work_rate?limit=10;descending=True'
+    var tweets_url = 'http://admin:password@127.0.0.1:5984/tweets_summary/_design/tweets_summary/_view/positive_rate?limit=10;descending=True'
+
+    // read 1
+    rp(aurin_url)
+        .then(function(response) {
+            var obj = JSON.parse(response);
+            for (var row in obj['rows']) {
+
+                var score = obj['rows'][row]['key'];
+                var suburb = obj['rows'][row]['value'];
+                suburbs_aurin.push(suburb);
+                scores_aurin.push(score);
+            }
+            // read 2
+            return rp(tweets_url)
+        })
+        .then(function(response) {
+            var obj = JSON.parse(response);
+            for (var row in obj['rows']) {
+
+                var score = obj['rows'][row]['key'];
+                var suburb = obj['rows'][row]['value'];
+                suburb_tweets.push(suburb);
+                scores_tweets.push(score);
+            }
+            res.render('volunteer', {
+                chart1: 'AURIN- Top 10 suburbs with highest voluntary work rate',
+                suburbs_aurin: JSON.stringify(suburbs_aurin),
+                scores_aurin: JSON.stringify(scores_aurin),
+                chart2: 'TWEETS- TOP 10 Cities with positive attitudes',
+                suburb_tweets: JSON.stringify(suburb_tweets),
+                scores_tweets: JSON.stringify(scores_tweets)
+            });
+            return Promise.resolve();
+
+        }).catch(function(err) { console.log("error: " + err) });
 });
 
 //for Trump page
@@ -239,7 +342,49 @@ router.get('/scenarios/volunteer', function(req, res, next) {
 //           2. tweets- trump negative
 router.get('/scenarios/trump', function(req, res, next) {
 
-    res.render('trump', { title: 'trump' });
+
+    var suburbs_pos = []
+    var scores_pos = []
+    var suburb_neg = []
+    var scores_neg = []
+
+    var trump_pos_url = 'http://admin:password@127.0.0.1:5984/tweets_summary/_design/tweets_summary/_view/trump_positive_rate?limit=10;descending=True'
+    var trump_neg_url = 'http://admin:password@127.0.0.1:5984/tweets_summary/_design/tweets_summary/_view/trump_negative_rate?limit=10;descending=True'
+
+    // read 1
+    rp(trump_pos_url)
+        .then(function(response) {
+            var obj = JSON.parse(response);
+            for (var row in obj['rows']) {
+
+                var score = obj['rows'][row]['key'];
+                var suburb = obj['rows'][row]['value'];
+                suburbs_pos.push(suburb);
+                scores_pos.push(score);
+            }
+            // read 2
+            return rp(trump_neg_url)
+        })
+        .then(function(response) {
+            var obj = JSON.parse(response);
+            for (var row in obj['rows']) {
+
+                var score = obj['rows'][row]['key'];
+                var suburb = obj['rows'][row]['value'];
+                suburb_neg.push(suburb);
+                scores_neg.push(score);
+            }
+            res.render('trump', {
+                chart1: 'AURIN- Top 10 suburbs with highest positive rate of Donald Trump',
+                suburbs_pos: JSON.stringify(suburbs_pos),
+                scores_pos: JSON.stringify(scores_pos),
+                chart2: 'TWEETS- TOP 10 Cities with highest negative rate of Donald Trump',
+                suburb_neg: JSON.stringify(suburb_neg),
+                scores_neg: JSON.stringify(scores_neg)
+            });
+            return Promise.resolve();
+
+        }).catch(function(err) { console.log("error: " + err) });
 });
 
 //Map
--- tweetsanalysis/GetProcessedData/voluntary_work.py
+++ tweetsanalysis/GetProcessedData/voluntary_work.py
@@ -3,9 +3,9 @@ import couchdb
 couch = couchdb.Server()
 dataset_db = couch['dataset_community_strength']
 
-#get top 10 suburbs with highest voluntary work rate
-dataset_results = dataset_db.view('voluntary_work_analysis/voluntary_work_rate', limit = 10, descending = True)
+# get top 10 suburbs with highest voluntary work rate
+dataset_results = dataset_db.view(
+    'voluntary_work_analysis/voluntary_work_rate', limit=10, descending=True)
 for row in dataset_results:
-	print row.key
-	print row.value
-	
+    print row.key
+    print row.value
