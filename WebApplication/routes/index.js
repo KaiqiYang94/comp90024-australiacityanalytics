d105ee170e8bba861a8ed26b74de6a79b7437ef2
Xing Hu <xinghu0164@gmail.com>
2017-05-07 18:01:53 +1000

    ier and ieo pages finished

--- WebApplication/routes/index.js
+++ WebApplication/routes/index.js
@@ -88,7 +88,7 @@ router.get('/scenarios', function(req, res, next) {
                 chart3: 'AURIN- TOP 10 Cities with highest Life Satisfaction (Low)',
                 suburbs_low: JSON.stringify(suburbs_low),
                 scores_low: JSON.stringify(scores_low),
-                chart4: 'AURIN- TOP 10 Cities with positive tweets',
+                chart4: 'Tweets- TOP 10 Cities with positive tweets',
                 suburb_tweets: JSON.stringify(suburb_tweets),
                 scores_tweets: JSON.stringify(scores_tweets)
             });
@@ -103,8 +103,49 @@ router.get('/scenarios', function(req, res, next) {
 //data need: 1. aurin- 
 //           2. tweets- semantic
 router.get('/scenarios/ieo', function(req, res, next) {
+    var suburbs_aurin = []
+    var scores_aurin = []
+    var suburb_tweets = []
+    var scores_tweets = []
 
-    res.render('ieo', { title: 'IEO' });
+    var aurin_url = 'http://admin:password@127.0.0.1:5984/dataset_ieo/_design/ieo_analysis/_view/ieo_score?limit=10;descending=True'
+    var tweets_url = 'http://admin:password@127.0.0.1:5984/tweets_summary/_design/tweets_summary/_view/positive_rate?limit=10;descending=True'
+
+    // read 1
+    rp(aurin_url)
+        .then(function(response) {
+            var obj = JSON.parse(response);
+            for (var row in obj['rows']) {
+
+                var score = obj['rows'][row]['key'];
+                var suburb = obj['rows'][row]['value'];
+                suburbs_aurin.push(suburb);
+                scores_aurin.push(score);
+            }
+            // read 2
+            return rp(tweets_url)
+        })
+        .then(function(response) {
+            var obj = JSON.parse(response);
+            for (var row in obj['rows']) {
+
+                var score = obj['rows'][row]['key'];
+                var suburb = obj['rows'][row]['value'];
+                suburb_tweets.push(suburb);
+                scores_tweets.push(score);
+            }
+            res.render('ieo', {
+                chart1: 'AURIN- Top 10 suburbs with highest education/ocupation score',
+                suburbs_aurin: JSON.stringify(suburbs_aurin),
+                scores_aurin: JSON.stringify(scores_aurin),
+                chart2: 'TWEETS- TOP 10 Cities with positive attitudes',
+                suburb_tweets: JSON.stringify(suburb_tweets),
+                scores_tweets: JSON.stringify(scores_tweets)
+            });
+            return Promise.resolve();
+
+        }).catch(function(err) { console.log("error: " + err) });
+
 });
 
 //for IER(Wealth) page
@@ -113,7 +154,68 @@ router.get('/scenarios/ieo', function(req, res, next) {
 //           3. tweets- negative
 router.get('/scenarios/ier', function(req, res, next) {
 
-    res.render('ier', { title: 'IER' });
+    var suburbs_aurin = []
+    var scores_aurin = []
+    var suburb_pos_tweets = []
+    var suburb_neg_tweets = []
+    var scores_pos_tweets = []
+    var scores_neg_tweets = []
+
+    var aurin_url = 'http://admin:password@127.0.0.1:5984/dataset_ier/_design/ier_analysis/_view/wealth_score?limit=10;descending=True'
+    var tweets_pos_url = 'http://admin:password@127.0.0.1:5984/tweets_summary/_design/tweets_summary/_view/positive_rate?limit=10;descending=True'
+    var tweets_neg_url = 'http://admin:password@127.0.0.1:5984/tweets_summary/_design/tweets_summary/_view/negative_rate?limit=10;descending=True'
+
+    // read 1
+    rp(aurin_url)
+        .then(function(response) {
+            var obj = JSON.parse(response);
+            for (var row in obj['rows']) {
+
+                var score = obj['rows'][row]['key'];
+                var suburb = obj['rows'][row]['value'];
+                suburbs_aurin.push(suburb);
+                scores_aurin.push(score);
+            }
+            // read 2
+            return rp(tweets_pos_url)
+        })
+        .then(function(response) {
+            var obj = JSON.parse(response);
+            for (var row in obj['rows']) {
+
+                var score = obj['rows'][row]['key'];
+                var suburb = obj['rows'][row]['value'];
+                suburb_pos_tweets.push(suburb);
+                scores_pos_tweets.push(score);
+            }
+            // read 2
+            return rp(tweets_neg_url)
+        })
+
+    .then(function(response) {
+        var obj = JSON.parse(response);
+        for (var row in obj['rows']) {
+
+            var score = obj['rows'][row]['key'];
+            var suburb = obj['rows'][row]['value'];
+            suburb_neg_tweets.push(suburb);
+            scores_neg_tweets.push(score);
+        }
+        res.render('ier', {
+            chart1: 'AURIN- Top 10 suburbs with highest wealth score',
+            suburbs_aurin: JSON.stringify(suburbs_aurin),
+            scores_aurin: JSON.stringify(scores_aurin),
+            chart2: 'TWEETS- TOP 10 Cities with highest positive attitudes',
+            suburb_pos_tweets: JSON.stringify(suburb_pos_tweets),
+            scores_pos_tweets: JSON.stringify(scores_pos_tweets),
+            chart3: 'TWEETS- TOP 10 Cities with highest negative attitudes',
+            suburb_neg_tweets: JSON.stringify(suburb_neg_tweets),
+            scores_neg_tweets: JSON.stringify(scores_neg_tweets)
+        });
+        return Promise.resolve();
+
+    }).catch(function(err) { console.log("error: " + err) });
+
 });
 
 //for health page
